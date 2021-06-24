c8ba34b3321c931f2b4d6d3c50501743fe8a5bd6
marekbugalski <marekbugalski@gmail.com>
2021-06-24 21:49:54 +0200

    end of 1.py

---
+++ Classification/1.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+
+df = pd.read_csv('https://sololearn.com/uploads/files/titanic.csv')
+df['male'] = df['Sex'] == 'male'
+X = df[['Pclass', 'male', 'Age', 'Siblings/Spouses', 'Parents/Children', 'Fare']].values
+y = df['Survived'].values
+
+model = LogisticRegression()
+model.fit(X, y)
+print(model.coef_, model.intercept_)
+print(model.predict(X[:25]))
+print(y[:25])
+print(model.score(X, y))
